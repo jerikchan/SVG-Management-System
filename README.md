1bbfc4c4b091e913ac45bbd9bcb0abe3a965ba02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-26 10:50:26 +0000

    Deploy jerikchan/SVG-Management-System to github.com/jerikchan/SVG-Management-System.git:gh-pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# SVG-Management-System
-
-SVG as a Service
-
-see [demo](//jerikchan.github.io/SVG-Management-System/dist/index.html)
--- vendors.97872f54fca929ac39ca.js
+++ vendors.97872f54fca929ac39ca.js
@@ -1,3 +1,155 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "7zGh":
+/***/ (function(module, exports) {
+
+
+;(function(FUNC) {
+    // 需要动画的 SVG 标签
+    const ANIME_TAGS = ['path', 'line'];
+
+    /**
+     * 获取 SVG 动画标签的选择器
+     * @param {String} SVGId SVG id
+     */
+    const getSVGAnimeTargets = (SVGId) => {
+        return ANIME_TAGS.map(tag => `#${SVGId} ${tag}`);
+    };
+
+    /**
+     * 运行 SVG 动画
+     * @param {Array} targets 选择器参数
+     */
+    const runSVGAnime = (targets) => {
+        anime({
+            targets,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 1500,
+            delay: function(el, i) { return i * 250 },
+            loop: false
+        });
+    };
+
+    /**
+     * 运行 SVG 动画
+     * @param {String} SVGId SVG id
+     */
+    FUNC.run = (SVGId) => {
+        runSVGAnime(getSVGAnimeTargets(SVGId));
+    };
+
+})(window.Site.SVGAnime || (window.Site.SVGAnime = {}));
+
+/***/ }),
+
+/***/ "KHd+":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return normalizeComponent; });
+/* globals __VUE_SSR_CONTEXT__ */
+
+// IMPORTANT: Do NOT use ES2015 features in this file (except for modules).
+// This module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle.
+
+function normalizeComponent (
+  scriptExports,
+  render,
+  staticRenderFns,
+  functionalTemplate,
+  injectStyles,
+  scopeId,
+  moduleIdentifier, /* server only */
+  shadowMode /* vue-cli only */
+) {
+  // Vue.extend constructor export interop
+  var options = typeof scriptExports === 'function'
+    ? scriptExports.options
+    : scriptExports
+
+  // render functions
+  if (render) {
+    options.render = render
+    options.staticRenderFns = staticRenderFns
+    options._compiled = true
+  }
+
+  // functional template
+  if (functionalTemplate) {
+    options.functional = true
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = 'data-v-' + scopeId
+  }
+
+  var hook
+  if (moduleIdentifier) { // server build
+    hook = function (context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+      // 2.2 with runInNewContext: true
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__
+      }
+      // inject component styles
+      if (injectStyles) {
+        injectStyles.call(this, context)
+      }
+      // register component module identifier for async chunk inferrence
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier)
+      }
+    }
+    // used by ssr in case component is cached and beforeCreate
+    // never gets called
+    options._ssrRegister = hook
+  } else if (injectStyles) {
+    hook = shadowMode
+      ? function () { injectStyles.call(this, this.$root.$options.shadowRoot) }
+      : injectStyles
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // for template-only hot-reload because in that case the render fn doesn't
+      // go through the normalizer
+      options._injectStyles = hook
+      // register for functioal component in vue file
+      var originalRender = options.render
+      options.render = function renderWithStyleInjection (h, context) {
+        hook.call(context)
+        return originalRender(h, context)
+      }
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate
+      options.beforeCreate = existing
+        ? [].concat(existing, hook)
+        : [hook]
+    }
+  }
+
+  return {
+    exports: scriptExports,
+    options: options
+  }
+}
+
+
+/***/ }),
+
+/***/ "PbT/":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
 /*
  * anime.js v3.1.0
  * (c) 2019 Julian Garnier
@@ -5,7 +157,7 @@
  * animejs.com
  */
 
-'use strict';
+
 
 // Defaults
 
@@ -1280,3 +1432,9 @@ anime.penner = penner;
 anime.random = function (min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; };
 
 module.exports = anime;
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=vendors.97872f54fca929ac39ca.js.map
